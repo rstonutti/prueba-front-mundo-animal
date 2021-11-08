bb42b721a9d2f0f81e46df6e2290816c1e734600
Rodrigo <rstonutti@gmail.com>
2021-11-08 19:34:31 -0300

    Card para muestra de datos en proceso

--- src/components/App/AdopcionScreen.js
+++ src/components/App/AdopcionScreen.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Card from '../layout/Card'
 
 export const AdopcionScreen = () => {
     return (
         <div>
             <h1>AdopcionScreen</h1>
+            <Card/>
         </div>
     )
 }
---
+++ src/components/layout/Card.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div className='row'>
+            <a className="card col" href="/#">
+                {/* <img src="..." className="card-img-top" alt="..."/> */}
+                <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </a>
+            <a className="card col" href="/#">
+                {/* <img src="..." className="card-img-top" alt="..."/> */}
+                <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </a>
+            <a className="card col" href="/#">
+                {/* <img src="..." className="card-img-top" alt="..."/> */}
+                <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </a>
+            <a className="card col" href="/#">
+                {/* <img src="..." className="card-img-top" alt="..."/> */}
+                <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </a>
+            <a className="card col" href="/#">
+                {/* <img src="..." className="card-img-top" alt="..."/> */}
+                <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </a>
+        </div>
+    );
+};
+
+export default Card
